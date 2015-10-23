d178870fd9db9e3a70df97f41d192f5d920b9744
Nichollas Fonseca <Nichollas@MacBook-Pro-de-Nichollas.local>
2015-10-22 22:52:09 -0300

    Fist commit, initial xcode project.

---
+++ PlacaresNFL/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlacaresNFL
+//
+//  Created by Nichollas Fonseca on 22/10/15.
+//  Copyright © 2015 Nichollas Fonseca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
